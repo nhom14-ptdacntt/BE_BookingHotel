113402d93f442b30015fa1ddb59791e507f5fe65
huyhoang <tranhuyyhoang2004@gmail.com>
2024-09-20 22:32:52 +0700

    build: build base project

---
+++ nhom14/src/main/java/com/bookingHotel/nhom14/Nhom14Application.java
@@ -0,0 +1,13 @@
+package com.bookingHotel.nhom14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nhom14Application.class, args);
+	}
+
+}
---
+++ nhom14/src/test/java/com/bookingHotel/nhom14/Nhom14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookingHotel.nhom14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nhom14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
